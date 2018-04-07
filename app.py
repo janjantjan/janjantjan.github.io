1bd3cddde42cc066714b5c84e949158d55a7d8a4
janjantjan <janjan.tjan@gmail.com>
2018-04-07 12:09:38 -0400

    app.py copied

--- app.py
+++ app.py
@@ -5,3 +5,36 @@ import requests
 app = Flask(__name__)
 
 api_url = 'http://borough-data.herokuapp.com'
+
+@app.route('/')
+def home():
+	api_data = requests.get(api_url + '/borough').json()['data']
+	return render_template('home.html', boroughs=api_data)
+
+@app.route('/view/borough/<int:id>')
+def render_borough_view(id):
+	api_data = requests.get(api_url + '/borough/' + str(id)).json()['data']
+	return render_template(
+		'borough_view.html',
+		name=api_data['name'],
+		county_name_value=api_data['county_name'],
+		area_value=api_data['area_sq_mi'],
+		population_value=api_data['population']
+	)
+
+@app.route('/combined')
+def render_combined_view():
+	i = 1
+	combined_data = []
+	while i <= 5:
+		api_data = requests.get(api_url + '/borough/' + str(i)).json()['data']
+		i = i + 1
+		combined_data.append(api_data)
+	return render_template(
+		'combined.html',
+		boroughs=combined_data
+	)
+
+if __name__ == "__main__":
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host="0.0.0.0", port=port, threaded=True)
